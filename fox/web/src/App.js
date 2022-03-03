368c7233393d81dd8fd0bcc5421f522164b6b059
jackXu <xujinzhang@372163.com>
2022-03-03 11:11:15 +0800

    web init

---
+++ fox/web/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">说明</Button>
+  </div>
+);
+
+export default App;
